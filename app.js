f415e2f3f2b8d7356735e5eff739ece0f099bb75
KP <kyleprock@verizon.net>
2022-08-05 11:26:49 -0500

    url fix

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: 'https://mighty-cliffs-46847.herokuapp.com'
 }))
 
 app.use('/', indexRouter);
