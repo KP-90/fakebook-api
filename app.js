02f9135b447b8aa5b4d2f45a9e6fe620ca78a836
KP <kyleprock@verizon.net>
2022-08-05 09:01:00 -0500

    h

--- app.js
+++ app.js
@@ -29,6 +29,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors())
 
+app.all('*', function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  //...
+ });
+
 
 app.use('/', indexRouter);
 
