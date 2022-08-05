876b67c251cc7800239af82ed5fdab7078693b61
KP <kyleprock@verizon.net>
2022-08-05 10:40:42 -0500

    h

--- app.js
+++ app.js
@@ -27,7 +27,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors({'Access-Control-Allow-Origin': '*'}))
+app.use(cors({
+  origin: 'http://localhost:3000'
+}))
 
 app.use('/', indexRouter);
 
