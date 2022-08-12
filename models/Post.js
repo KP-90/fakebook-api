67e2edbbda0f40e64041a88ca836366cc2cf1941
KP <kyleprock@verizon.net>
2022-08-12 11:49:11 -0500

    timestamps

--- models/Post.js
+++ models/Post.js
@@ -8,13 +8,13 @@ let post_schema = new Schema({
     post_contents: {type: String},
     likes: [{type: Schema.Types.ObjectId, ref: 'User'}],
 
-    date_created: {type: Date, default: Date.now()}
+    date_created: {type: Number, default: Date.now()}
 }, opts)
 
 post_schema
 .virtual('date_readable')
 .get(function(){
-    return DateTime.fromJSDate(this.date_created).toLocaleString(DateTime.DATETIME_FULL);
+    return DateTime.fromJSDate(new Date(this.date_created)).toLocaleString(DateTime.DATETIME_FULL);
 })
 
 module.exports = mongoose.model('Post', post_schema)
