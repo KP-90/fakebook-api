c1221c9bb92af32a1b47461f685427c973b34f26
KP <kyleprock@verizon.net>
2022-09-26 14:33:39 -0500

    test render.com

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ app.use((req, res, next) => {
   if (allowedOrigins.includes(origin)) {
        res.setHeader('Access-Control-Allow-Origin', origin);
   }
+  res.setHeader('Access-Control-Allow-Origin')
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', true);
   return next();
