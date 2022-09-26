e2f71ea9909c8122053e4b219e5f099737a0129a
KP <kyleprock@verizon.net>
2022-09-26 14:53:01 -0500

    fix url

--- app.js
+++ app.js
@@ -27,22 +27,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-/*
+
 app.use(cors({
-  origin: [process.env.REACT_APP_ORIGINS]
+
 }))
-*/
-app.use((req, res, next) => {
-  const allowedOrigins = [process.env.REACT_APP_ORIGINS];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-       res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  res.setHeader('Access-Control-Allow-Origin')
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', true);
-  return next();
-});
 
 app.use('/', indexRouter);
 
